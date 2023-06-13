7f891b510d6b4b8241707c38bec048c00654e577
Bis92 <bis920309@gmail.com>
2023-06-14 00:43:38 +0900

    page item container add

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,7 +1,30 @@
-import { BaseComponent } from '../component.js';
+import { BaseComponent, Component } from '../component.js';
 
-export class PageComponent extends BaseComponent<HTMLUListElement> {
+export interface Composable {
+  addChild(child: Component): void;
+}
+
+class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
   constructor() {
-    super('<ul class="page">This is PageComponent!</ul>')
+    super(`<li class="page_item">
+            <section class="page-item__body"></section>
+            <button class="close">&times;</button>
+          </li>`);
+  }
+  addChild(child: Component) {
+    const container = this.element.querySelector('.page-item__body')! as HTMLElement;
+    child.attachTo(container);
+  }
+}
+
+export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
+  constructor() {
+    super('<ul class="page"></ul>')
+  }
+
+  addChild(section: Component) {
+    const item = new PageItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, 'beforeend');
   }
 }
